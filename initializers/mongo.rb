810dccc9f7fbec543077db13c709d9c5829db624
Chris Winslett <chris@mongohq.com>
2012-02-26 18:36:42 -0600

    Converted logging to standard out

--- initializers/mongo.rb
+++ initializers/mongo.rb
@@ -2,5 +2,5 @@ require 'uri'
 
 db_name = $1 if ENV['MONGOHQ_URL'] =~ /\/([^\/]+)$/
 
-$conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'], :logger => Logger.new(File.dirname(__FILE__) + "/../logs/mongo.yml"))
+$conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'], :logger => Logger.new(STDOUT))
 $db = $conn.db(db_name)
